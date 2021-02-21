7c7f8a738a23e9f2207dab1c23c15bfe01cda680
oksongh <oksongh@gmail.com>
2021-02-21 21:41:08 +0900

    modify using temperature.update function

--- code/src/input.cpp
+++ code/src/input.cpp
@@ -11,9 +11,6 @@ const InputContainer& input() {
     con.common_.now_ms = millis();
     con.message = readSerial();
 
-    temperature.update();
-    con.common_.current_tempT0 = temperature.T0;
-    con.common_.current_tempT1 = temperature.T1;
-    con.common_.current_tempT2 = temperature.T2;
+    temperature.update(con.common_.current_tempT0, con.common_.current_tempT1, con.common_.current_tempT2);
     return con;
 }

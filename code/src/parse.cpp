ff3901e7d68ff43cd326dd3adabe0bae68d3679c
oksongh <oksongh@gmail.com>
2021-02-21 21:39:48 +0900

    add clearBuffer and modification split

--- code/src/parse.cpp
+++ code/src/parse.cpp
@@ -1,43 +1,22 @@
 #include "parse.hpp"
 
-// template <int BUF_SIZE>
-// Parser<BUF_SIZE>::Parser(const char delim) {
-//     delim_ = delim;
-// }
-
-// return operator ,operand as parameter
-// template <int bufsize>
-// bool Parser<bufsize>::parse(const String src, String& operator_, double* operand, const int operand_bufsize) {
-//     int buf_assined_size = split(src, buf, bufsize, delim_);
-//     if (buf_assined_size > bufsize || buf_assined_size > operand_bufsize + 1) {
-//         operator_ = "";
-//         return false;
-//     }
-//     operator_ = buf[0];
-
-//     for (int i = 0; i < operand_size && i + 1 < buf_assined_size; i++) {
-//         if (!strTodError(buf[i + 1], operand[i])) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
 // if succeed return true,if not ()
 // return size of assigned to dst
 int split(const String src, String* dst, const int dstsize, const char delim) {
-    int i, j = 0;
+    int array_ind, char_ind;
+    array_ind = char_ind = 0;
 
     const int srclen = src.length();
-    for (; j < srclen; j++) {
-        if (src[j] == delim) {
-            i++;
+    for (; char_ind < srclen; char_ind++) {
+        if (src[char_ind] == delim) {
+            array_ind++;
         } else {
-            if (i < dstsize) {
-                dst[i] += src[j];
+            if (array_ind < dstsize) {
+                dst[array_ind] += src[char_ind];
             }
         }
     }
-    return i;
+    return array_ind + 1;
 }
 bool strToDoubleError(const String s, double& d) {
     char* tmperr;
--- code/src/parse.hpp
+++ code/src/parse.hpp
@@ -12,7 +12,23 @@ struct ParseResult {
     const unsigned int max_operand_size = OPERAND_BUF_SIZE;
     double operand[OPERAND_BUF_SIZE];
 
-    bool parsable = false;
+    bool parsable;
+    void init() {
+        operand_size = 0;
+        parsable = false;
+        for (unsigned int i = 0; i < max_operand_size; i++) {
+            operand[i] = 0.0;
+        }
+    }
+    String toString() {
+        String s = "parseResult:";
+        s += operator_ + " ";
+        for (unsigned int i = 0; i < max_operand_size; i++) {
+            s += String(operand[i]) + " ";
+        }
+        s += "oprandsize " + String(operand_size);
+        return s;
+    }
 };
 
 template <int OPEAND_BUF_SIZE>
@@ -26,12 +42,13 @@ class Parser {
    public:
     explicit Parser(const char delim) {
         delim_ = delim;
+        parse_result.init();
     }
 
     // return operator ,operand as parameter
     const ParseResult<OPEAND_BUF_SIZE>& parse(const String src) {
-        // parse(const String src, String& operator_, double* operand, const int operand_bufsize) {
-
+        clearBuffer();
+        Serial.println("src" + src);
         unsigned int buf_assined_size = split(src, str_buf, str_buf_size, delim_);
 
         // check assigned size
@@ -54,6 +71,14 @@ class Parser {
 
         parse_result.parsable = true;
         return parse_result;
+    }
+
+   private:
+    void clearBuffer() {
+        parse_result.init();
+        for (unsigned int i = 0; i < str_buf_size; i++) {
+            str_buf[i] = "";
+        }
     }
 };
 // return operator ,operand

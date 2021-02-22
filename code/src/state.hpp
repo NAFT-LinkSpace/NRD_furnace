9b92f6c0b00c3bfde4719388dc8764345abca099
oksongh <oksongh@gmail.com>
2021-02-23 05:17:58 +0900

    use tostringhelper & bug fix about elasped time

--- code/src/state.hpp
+++ code/src/state.hpp
@@ -47,16 +47,24 @@ class Control : public State {
 
    private:
     double duty(const unsigned long now_ms, const double skip_time_s, const double T0, const double T1, const double T2) {
-        const double target_temp = targetFunction(now_ms - static_cast<unsigned long>(skip_time_s / 1e3), is_control_finished);
+        const double elasped_time_s = now_ms / 1e3 - skip_time_s;
+        const double target_temp = targetFunction(elasped_time_s, is_control_finished);
+
         if (is_control_finished) {
-            Serial.print("end control");
+            Serial.println("end control");
+            delay(1000);
             return 0.0;
         }
-        const double out_W = vpid.output(target_temp - (feedbackTemperature(T0, T1, T2)), now_ms);
+        const double error = target_temp - feedbackTemperature(T0, T1, T2, elasped_time_s);
+        const double out_W = vpid.output(error, elasped_time_s);
         const double duty_per = out_W / MAX_OUTPUT_ENERGY_W * 100;
-
-        Serial.println("out_W" + String(out_W));
-        Serial.println("duty" + String(duty_per));
+        if (now_ms % 1000 < 5) {
+            Serial.println(
+                toStringHelper("error", error) +
+                toStringHelper("target_temperature", target_temp) +
+                toStringHelper("out_W", out_W) +
+                toStringHelper("duty_per", duty_per));
+        }
         return duty_per;
     }
 };
@@ -65,6 +73,7 @@ class ConstPWM : public State {
    public:
     void output(const OutputContainer& out) override {
         setLongPeriodPWM(out.duty_per, PWM_PERIOD_ms, out.common_.now_ms);
+        // setPWM(out.duty_per);
     }
     const String toString() const override {
         return toStringHelper("State", "ConstPWM");

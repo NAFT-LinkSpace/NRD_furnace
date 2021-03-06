963e114762a33a0a1e54e2540b1e8c6b85a9eb53
oksongh <oksongh@gmail.com>
2021-03-06 20:17:51 +0900

    change pinassign for board

--- code/src/pin.hpp
+++ code/src/pin.hpp
@@ -2,12 +2,12 @@
 #define PIN_HEADER_FILE
 // namespace PINS {
 
-constexpr int DO = 4;
+constexpr int DO = 7;
 constexpr int CS0 = 6;
 constexpr int CS1 = 5;
-constexpr int CS2 = 7;
+constexpr int CS2 = 4;
 constexpr int CLK = 3;
-constexpr int PWMOUT = 13;
+constexpr int PWMOUT = 11;
 // constexpr int BUTTON = 8;
 
 // }  // namespace PINS

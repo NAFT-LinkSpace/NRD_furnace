ad2a546278abde59c9f1b98fb269f2cf58d7f7cb
oksongh <oksongh@gmail.com>
2021-04-20 21:12:21 +0900

    rm muda cpp

--- code/src/state.cpp
+++
@@ -1,7 +0,0 @@
-// #include "state.hpp"
-// void State::outputHelper(const OutputContainer& out) {
-//     Serial.print(out.toString());
-//     output(out);
-//     Serial.println();
-//     return;
-// }

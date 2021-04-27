0a930a6fb5dfe777751174d13c1ef2df49d9739e
oksongh <oksongh@gmail.com>
2021-04-28 03:09:39 +0900

    rm commments

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -9,19 +9,9 @@
 void setup() {
     inputBegin();
 }
-// unsigned long pret = 0;
-// unsigned long t = 0;
 
 void loop() {
     // put your main code here, to run repeatedly:
-    // t = millis();
 
-    auto input_con = input();
-    convert(input_con);
-    // Serial.println();
-    // if (t - pret < 0) {
-    //     delay(20 - (t - pret));
-    //     Serial.println(20 - (t - pret));
-    // }
-    // pret = t;
+    convert(input());
 }
